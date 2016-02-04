71be2f74dc76cade4810181029e9ad01213aaad6
Jared Rummler <jared@jaredrummler.com>
2016-02-04 00:54:03 -0800

    remove navigation header, set icon and text ColorStateList

--- app/src/main/java/com/jrummyapps/packagemanager/fragments/BusyBoxNavigationFragment.java
+++ app/src/main/java/com/jrummyapps/packagemanager/fragments/BusyBoxNavigationFragment.java
@@ -17,9 +17,7 @@
 
 package com.jrummyapps.packagemanager.fragments;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
@@ -32,51 +30,51 @@ import android.widget.ImageView;
 import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.jrummyapps.android.base.BaseDrawerActivity;
 import com.jrummyapps.android.base.BaseFragment;
-import com.jrummyapps.android.fileproperties.wallpaper.NowWallpaper;
+import com.jrummyapps.android.theme.ColorScheme;
 import com.jrummyapps.android.util.ResUtils;
 import com.jrummyapps.packagemanager.R;
-import com.squareup.picasso.Picasso;
-
-import java.util.Random;
 
 public class BusyBoxNavigationFragment extends BaseFragment implements NavigationView.OnNavigationItemSelectedListener {
 
+  private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+  private static final int[] DISABLED_STATE_SET = {-android.R.attr.state_enabled};
+  static final int[] EMPTY_STATE_SET = new int[0];
+
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return new NavigationView(getActivity());
+    return inflater.inflate(R.layout.busybox_navigation_view, container, false);
   }
 
   @Override public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    NavigationView navigationView = (NavigationView) view;
-    navigationView.getMenu().add(0, 1, 0, "Installer").setIcon(R.drawable.ic_package_down_white_24dp);
-    navigationView.getMenu().add(0, 2, 0, "Applets").setIcon(R.drawable.ic_applets_white_24dp);
-    navigationView.getMenu().add(0, 3, 0, "Scripts").setIcon(R.drawable.ic_code_array_white_24dp);
-    navigationView.getMenu().add(0, 3, 0, "Terminal").setIcon(R.drawable.ic_terminal_white_24dp);
+    NavigationView navigationView = findById(R.id.navigation_view);
+
+    navigationView.getMenu().findItem(R.id.action_installer).setChecked(true);
 
     ImageView headerView = new KenBurnsView(getActivity());
     headerView.setLayoutParams(new FrameLayout.LayoutParams(
         ViewGroup.LayoutParams.MATCH_PARENT,
         ResUtils.dpToPx(180)));
 
-    NetworkInfo networkInfo =
-        ((ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-    if (networkInfo != null && networkInfo.isConnected() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-      NowWallpaper[] wallpapers = NowWallpaper.values();
-      Random random = new Random();
-      NowWallpaper nowWallpaper = wallpapers[random.nextInt(wallpapers.length)];
-      int size = Math.min(1440, ResUtils.getScreenWidth());
-      String url = nowWallpaper.getUrl(NowWallpaper.getTimeOfDay(), size, size);
-      Picasso.with(getActivity())
-          .load(url)
-          .centerCrop()
-          .fit()
-          .error(R.drawable.materialviewpagerheader)
-          .into(headerView);
-    } else {
-      headerView.setImageResource(R.drawable.materialviewpagerheader);
-    }
+    navigationView.setItemIconTintList(new ColorStateList(new int[][]{
+        DISABLED_STATE_SET,
+        CHECKED_STATE_SET,
+        EMPTY_STATE_SET
+    }, new int[]{
+        ColorScheme.getAccent(),
+        ColorScheme.getAccent(),
+        ColorScheme.getSubMenuIcon()
+    }));
 
-    navigationView.addHeaderView(headerView);
+    navigationView.setItemTextColor(new ColorStateList(new int[][]{
+        DISABLED_STATE_SET,
+        CHECKED_STATE_SET,
+        EMPTY_STATE_SET
+    }, new int[]{
+        ColorScheme.getAccent(),
+        ColorScheme.getAccent(),
+        ColorScheme.getSubMenuIcon()
+    }));
+
     navigationView.setNavigationItemSelectedListener(this);
   }
 
@@ -85,6 +83,36 @@ public class BusyBoxNavigationFragment extends BaseFragment implements Navigatio
       BaseDrawerActivity activity = (BaseDrawerActivity) getActivity();
       activity.toggle();
     }
+
+    if (item.isChecked()) {
+      return true;
+    }
+
+    item.setChecked(true);
+
+    int itemId = item.getItemId();
+
+    switch (itemId){
+      case R.id.action_installer:
+        getActivity().getFragmentManager()
+            .beginTransaction()
+            .replace(R.id.content_frame, new BusyBoxInstallerFragment())
+            .commit();
+        return true;
+      case R.id.action_applets:
+        getActivity().getFragmentManager()
+            .beginTransaction()
+            .replace(R.id.content_frame, new BusyBoxAppletsFragment())
+            .commit();
+        return true;
+      case R.id.action_scripts:
+
+        return true;
+      case R.id.action_terminal:
+
+        return true;
+    }
+
     return true;
   }
 
