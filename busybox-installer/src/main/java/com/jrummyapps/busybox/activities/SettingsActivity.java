12b19c41fe32a1ffcf76fc79ed54b878281cc3bd
Jared Rummler <jared@jaredrummler.com>
2016-02-18 16:26:14 -0800

    add easter egg

--- busybox-installer/src/main/java/com/jrummyapps/busybox/activities/SettingsActivity.java
+++ busybox-installer/src/main/java/com/jrummyapps/busybox/activities/SettingsActivity.java
@@ -17,14 +17,32 @@
 
 package com.jrummyapps.busybox.activities;
 
+import android.app.Activity;
 import android.app.Fragment;
 
+import com.jrummyapps.android.common.Toasts;
+import com.jrummyapps.android.easteregg.EasterEggCallback;
 import com.jrummyapps.android.preferences.activities.MainPreferenceActivity;
 import com.jrummyapps.busybox.R;
 import com.jrummyapps.busybox.fragments.AboutFragment;
 import com.jrummyapps.busybox.fragments.SettingsFragment;
 
-public class SettingsActivity extends MainPreferenceActivity {
+import java.util.Random;
+
+public class SettingsActivity extends MainPreferenceActivity implements EasterEggCallback {
+
+  private static final String[] INSULTS = {
+      "I'm not saying I hate you, but I would unplug your life support to charge my phone.",
+      "I bet your brain feels as good as new, seeing that you never use it.",
+      "You bring everyone a lot of joy, when you leave the room.",
+      "Two wrongs don't make a right, take your parents as an example.",
+      "If laughter is the best medicine, your face must be curing the world.",
+      "You're the reason the gene pool needs a lifeguard.",
+      "I don't exactly hate you, but if you were on fire and I had water, I'd drink it.",
+      "You're as bright as a black hole, and twice as dense.",
+      "If you spoke your mind, you'd be speechless.",
+      "Don't feel sad, don't feel blue, Frankenstein was ugly too."
+  };
 
   @Override protected Fragment getFragment(int position) {
     int stringId = getStringId(position);
@@ -34,6 +52,11 @@ public class SettingsActivity extends MainPreferenceActivity {
       return new AboutFragment();
     }
     return super.getFragment(position);
+  }
+
+  @Override public void onRequestEgg(Activity activity, int id, int count) {
+    // insult our most valued customers for finding our precious easter egg
+    Toasts.show(INSULTS[new Random().nextInt(INSULTS.length)]);
   }
 
 }
