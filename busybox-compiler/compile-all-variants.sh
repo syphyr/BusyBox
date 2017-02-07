f0fc491124d9e5131582557f92629f9092f300bf
syphyr <syphyr@gmail.com>
2017-02-07 22:39:50 +0100

    Build only static arm

--- busybox-compiler/compile-all-variants.sh
+++ busybox-compiler/compile-all-variants.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for arch in arm arm64 x86 mips
-do
-  for arg in pie nopie static
-  do
-    ./compile-busybox.sh $arch $arg
-  done
-done
+#for arch in arm arm64 x86 mips
+#do
+#  for arg in pie nopie static
+#  do
+#    ./compile-busybox.sh $arch $arg
+#  done
+#done
+
+./compile-busybox.sh arm static
